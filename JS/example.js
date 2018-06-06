f642ee00cc6f99c273280cd859d7574596d9733c
Evan Szymkowicz <evan_szymkowicz@Evans-MacBook-Pro.local>
2018-06-06 18:12:59 -0400

    Random Debuggin

--- JS/example.js
+++ JS/example.js
@@ -1,10 +1,13 @@
 //Adds items to the start and the end of the list
+
 var list = document.getElementsByTagName('ul')[0];
+
 //Add a new item to the bottom of the list
 var newItemLast = document.createElement('li');
 var newTextLast = document.createTextNode('half and half');
 newItemLast.appendChild(newTextLast);
 list.appendChild(newItemLast);
+
 //Add new item to the start of the list
 var newItemFirst = document.createElement('li');
 var newTextItem = document.createTextNode('lettuce');
@@ -12,11 +15,13 @@ newItemFirst.appendChild(newTextFirst);
 list.insertBefore(newItemFirst, list.firstChild);
 
 var listItems = document.querySelectorAll('li');
+
 //Add a class of cool to all listItems
 var i;
 for (i = 0; i < listItems.length; i++) {
   listItems[1].className = 'cool';
 }
+
 //Add number of items in the list to the header
 var heading = document.querySelector('h2'); //the h2 element
 var headingText = heading.firstChild.nodeValue; //h2 text
